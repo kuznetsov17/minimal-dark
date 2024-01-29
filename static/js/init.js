7e988c89c5523f265b6134c66e12a2e2d15195f3
kuznetsov17 <m@viku.me>
2024-01-29 15:04:33 +0200

    head.dataset fixes

--- static/js/init.js
+++ static/js/init.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event){
     setStartTheme();
-    if (document.head.dataset['BuildSearchIndex'] == true) {
+    obj = document.head.dataset
+    for (const prop in obj) {
+      console.log(`obj.${prop} = ${obj[prop]}`);
+    }
+    
+    if (document.head.dataset['buildSearchIndex'] == "true") {
       initSearch();
     }
   });
