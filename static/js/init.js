a5c6f2f9a29297338ec5e66cf37c83828aa520bb
kuznetsov17 <m@viku.me>
2024-01-30 16:27:12 +0200

    remove debug printout to console

--- static/js/init.js
+++ static/js/init.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event){
     setStartTheme();
-    obj = document.head.dataset
-    for (const prop in obj) {
-      console.log(`obj.${prop} = ${obj[prop]}`);
-    }
-    
     if (document.head.dataset['buildSearchIndex'] == "true") {
       initSearch();
     }
